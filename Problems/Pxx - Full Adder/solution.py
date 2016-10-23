ba335e82fb0743f161c36650df32b403516f1f58
Benito Palacios Sánchez <benito356@gmail.com>
2016-10-23 12:22:37 +0200

    Improve performance

--- Problems/Pxx - Full Adder/solution.py
+++ Problems/Pxx - Full Adder/solution.py
@@ -1,45 +1,31 @@
-#!/bin/python
-# -*- coding: utf-8 -*-
+"""Solution to problem of IEEEXtreme 10.0."""
 
 
 def convertir(base, letras, value):
     salida = 0
 
-
-    value = value.replace(" ","")
-    value = value.replace("+","")
-
-    i = len(value)-1
-
+    value = value.strip("+ ")
+    i = len(value) - 1
     for dig in value:
-        if not dig=='' and not dig=='+' and not dig==' ':
-            salida+=letras.index(dig)*base**i
-            i-=1
+        salida += letras.index(dig) * (base ** i)
+        i -= 1
+
     return salida
 
 
-def int2base(x, base,digs):
-  if x < 0: sign = -1
-  elif x == 0: return digs[0]
-  else: sign = 1
-  x *= sign
-  digits = []
-  while x:
-    digits.append(digs[x % base])
-    x /= base
-  if sign < 0:
-    digits.append('-')
-  digits.reverse()
-  return ''.join(digits)
+def int2base(x, base, digs):
+    digits = ''
+    while x:
+        digits = digs[x % base] + digits
+        x /= base
+    return digits
 
 
 def main():
-
     lenCadenas = 0
     base, letras = raw_input().split(" ")
 
-    print(base+" "+letras)
-
+    print(base + " " + letras)
     sumando1 = raw_input()
     lenCadenas = len(sumando1)
     print(sumando1)
@@ -48,20 +34,18 @@ def main():
     print(sumando2)
     print(raw_input())
 
-    result = raw_input()
+    raw_input()  # result
 
     base = int(base)
 
-    sumando1 = convertir(base,letras,sumando1)
-    sumando2 = convertir(base,letras,sumando2)
+    sumando1 = convertir(base, letras, sumando1)
+    sumando2 = convertir(base, letras, sumando2)
 
-    resultado = sumando1+sumando2
+    resultado = sumando1 + sumando2
 
     final = int2base(resultado, base, letras)
-    espacios = lenCadenas-len(final)
-
-    final = ' '*espacios+final
-
+    espacios = lenCadenas - len(final)
+    final = ' ' * espacios + final
     print(final)
 
 if __name__ == '__main__':
